1ee196bea4e51eb2d075b897bf6be111285941e9
akshatbakliwal08 <50566226+akshatbakliwal08@users.noreply.github.com>
2020-09-28 02:49:33 +0530

    A small change

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const User=require('./models/user');
 const Task=require('./models/task');
 const { update } = require('./models/user');
 const app=express();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT;
 const userRouter=require('./routers/user');
 const taskRouter=require('./routers/task');
 
